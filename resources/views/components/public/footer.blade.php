87045d6445524a933975fe69f99cab8e0cf0b32b
putradotid <aprildwi999@gmail.com>
2026-04-19 15:50:48 +0700

    Mengerjakan section pada page beranda public (belum selesai) dan perbaikan footer

--- resources/views/components/public/footer.blade.php
+++ resources/views/components/public/footer.blade.php
@@ -1,13 +1,20 @@
-<section class="px-8 py-12 text-center">
-  <p class="text-gray-500 mb-6">
-    Trusted by these companies
-  </p>
+<footer class="py-6">
+  <div class="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-sm text-gray-600">
 
-  <div class="flex flex-wrap justify-center gap-8 text-gray-700 font-medium">
-    <span>Transistor</span>
-    <span>Tuple</span>
-    <span>StaticKit</span>
-    <span>Mirage</span>
-    <span>Laravel</span>
+    <!-- Left -->
+    <div>
+      Copyright by ©putradotid 2026
+    </div>
+
+    <!-- Right -->
+    <div class="flex gap-6 mt-4 md:mt-0">
+      <a href="#" class="hover:text-gray-900 transition">
+        Privacy Policy
+      </a>
+      <a href="#" class="hover:text-gray-900 transition">
+        Terms of Use
+      </a>
+    </div>
+
   </div>
-</section>
+</footer>
--- resources/views/public/beranda.blade.php
+++ resources/views/public/beranda.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.public')
 
-@section('content')
-<section class="bg-gray-300 relative mt-16 px-8 py-16 md:py-24">
-
+@section('content')\
+{{-- Hero section --}}
+<section class="bg-gray-300 relative mt-10 px-8 py-16 md:py-24">
   <div class="max-w-2xl">
     <h1 class="text-4xl md:text-6xl font-bold leading-tight">
       Where Innovation
@@ -34,7 +34,8 @@
 
 </section>
 
-<section class="bg-amber-500 text-white py-20">
+{{-- Section 1 --}}
+<section class="bg-amber-500 text-white py-10">
   <div class="max-w-6xl mx-auto text-center px-6">
 
     <!-- Title -->
@@ -43,7 +44,7 @@
     </h2>
 
     <!-- Grid -->
-    <div class="mt-12 flex justify-center gap-16">
+    <div class="flex justify-center gap-16">
 
       <!-- Item -->
       <div class="flex flex-col items-center">
@@ -71,6 +72,165 @@
       </div>
 
     </div>
+  </div>
+</section>
+
+{{-- Section 2 --}}
+<section class="flex px-1 py-16 md:py-16">
+
+  <div class="grid grid-cols-3 gap-4 max-w-6xl mx-auto">
+
+    <!-- Gambar -->
+    <div class="grid grid-rows-3 gap-4">
+      
+      <img src="{{ asset('storage/assets/beranda.jpg') }}" 
+          class="w-30 h-30 object-cover">
+
+      <img src="{{ asset('storage/assets/beranda.jpg') }}" 
+          class="w-30 h-30 object-cover">
+          
+      <img src="{{ asset('storage/assets/beranda.jpg') }}" 
+          class="w-30 h-30 object-cover">
+
+    </div>
+
+    <!-- RIGHT SIDE (gambar besar) -->
+    <div class="col-span-2">
+      <img src="{{ asset('storage/assets/beranda.jpg') }}" 
+          class="w-auto h-150 object-cover">
+    </div>
+
+  </div>
+
+  {{-- Text --}}
+  <div class="max-w-2xl px-8">
+    <span class="text-amber-500 font-bold">Beranda</span>
+    <h1 class="text-4xl md:text-5xl font-bold leading-tight">
+      Mitra Strategis dalam Transformasi Digital
+    </h1>
+
+    <p class="mt-6 text-gray-600 text-xs md:text-sm">
+      PT Motekar Cipta Teknologi adalah perusahaan yang bergerak di bidang teknologi informasi, industri digital, aktivitas profesional dan teknis, serta pendidikan. Kami berkomitmen menghadirkan solusi teknologi yang inovatif, berorientasi pada kebutuhan pengguna, dan berkelanjutan. Dengan dukungan tim profesional dan kolaborasi strategis, kami membantu organisasi meningkatkan efisiensi, produktivitas, dan daya saing melalui implementasi sistem dan platform digital yang tepat guna. Kami senantiasa mengikuti perkembangan teknologi terkini untuk memastikan setiap solusi yang kami kembangkan relevan dan adaptif terhadap perubahan. Melalui pendekatan yang terstruktur dan berbasis kualitas, kami berupaya memberikan nilai tambah yang nyata bagi setiap mitra dan klien kami.
+    </p>
+
+    <div class="mt-6 flex gap-4">
+    <button class="bg-blue-900 text-white px-6 py-3 rounded-full hover:bg-blue-700 transition duration-300">
+      LIHAT LAYANAN
+    </button>
+  </div>
+  </div>
+
+</section>
+
+{{-- Section 3 --}}
+<section class="bg-amber-500 text-white py-14">
+  <div class="max-w-6xl mx-auto px-6">
+
+    <div class="flex justify-between text-center">
+
+      <!-- Item -->
+      <div class="flex flex-col items-center flex-1">
+        <h2 class="text-4xl md:text-6xl font-bold">190+</h2>
+        <p class="mt-2 text-xs md:text-sm font-semibold tracking-wide">
+          KLIEN TERLAYANI
+        </p>
+      </div>
+
+      <div class="flex flex-col items-center flex-1">
+        <h2 class="text-4xl md:text-6xl font-bold">460+</h2>
+        <p class="mt-2 text-xs md:text-sm font-semibold tracking-wide">
+          PROYEK SELESAI
+        </p>
+      </div>
+
+      <div class="flex flex-col items-center flex-1">
+        <h2 class="text-4xl md:text-6xl font-bold">230+</h2>
+        <p class="mt-2 text-xs md:text-sm font-semibold tracking-wide">
+          TENAGA PROFESIONAL
+        </p>
+      </div>
+
+      <div class="flex flex-col items-center flex-1">
+        <h2 class="text-4xl md:text-6xl font-bold">50+</h2>
+        <p class="mt-2 text-xs md:text-sm font-semibold tracking-wide">
+          MITRA & KOLABORASI
+        </p>
+      </div>
+
+    </div>
+
+  </div>
+</section>
+
+{{-- Section 4 --}}
+<section class="py-16 md:py-24">
+  <div class="max-w-6xl mx-auto px-6 flex flex-col md:flex-row items-center gap-10">
+
+    <!-- TEXT -->
+    <div class="flex-1">
+      <h1 class="text-4xl md:text-5xl font-bold leading-tight">
+        Where Innovation <br>
+        Meets Execution
+      </h1>
+
+      <p class="mt-6 text-gray-600 text-sm md:text-base">
+        Kami membantu perusahaan dan UMKM bertransformasi secara digital melalui pengembangan software, sistem terintegrasi, dan solusi teknologi yang scalable. 
+        Solusi teknologi terintegrasi untuk mempercepat pertumbuhan bisnis Anda di era digital.
+      </p>
+
+      <div class="mt-8">
+        <button class="bg-blue-900 text-white px-6 py-3 rounded-full hover:bg-blue-700 transition">
+          HUBUNGI KAMI
+        </button>
+      </div>
+    </div>
+
+    <!-- IMAGE -->
+    <div class="flex-1 flex justify-center">
+      <img 
+        src="{{ asset('storage/hero-logo.png') }}"
+        class="w-72 md:w-96 object-contain"
+        alt="hero"
+      />
+    </div>
+
+  </div>
+</section>
+
+{{-- Section 5 --}}
+<section class="bg-gray-100 py-16">
+  <div class="max-w-3xl mx-auto px-6">
+
+    <div class="bg-white rounded-2xl shadow-lg p-10 flex flex-col md:flex-row items-center gap-8">
+
+      <!-- IMAGE -->
+      <div class="w-24 md:w-32 flex">
+        <img src="{{ asset('storage/hero-logo.png') }}" 
+             class="w-full object-contain" />
+      </div>
+
+      <!-- CONTENT -->
+      <div class="flex-1 text-center md:text-left">
+
+        <h2 class="text-2xl md:text-3xl font-bold text-gray-900">
+          Butuh Konsultasi Teknologi?
+        </h2>
+
+        <p class="mt-4 text-gray-600 text-sm md:text-base leading-relaxed max-w-xl">
+          Diskusikan kebutuhan sistem atau solusi digital Anda bersama tim kami 
+          untuk menemukan solusi teknologi yang tepat bagi bisnis
+        </p>
+
+        <div class="mt-6">
+          <button class="bg-amber-500 hover:bg-amber-600 text-white px-8 py-3 rounded-lg font-semibold transition">
+            Jadwalkan Meeting
+          </button>
+        </div>
+
+      </div>
+
+    </div>
+
   </div>
 </section>
 @endsection
